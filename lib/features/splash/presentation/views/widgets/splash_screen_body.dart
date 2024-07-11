7aa6503d0e35d8b7064af397e38bb68decb57f47
anas <anas3ez@gmail.com>
2024-07-11 20:22:59 +0300

    center text

--- lib/features/splash/presentation/views/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -7,6 +7,8 @@ class SplashScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AppAssets.logo),
       ],

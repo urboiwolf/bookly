c7311c234ff4c7612efd20911ab584d7fc826177
anas <anas3ez@gmail.com>
2024-07-17 17:12:16 +0300

    adding book model

--- lib/features/home/presentaion/views/widgets/custom_appbar.dart
+++ lib/features/home/presentaion/views/widgets/custom_appbar.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/core/utlis/my_assets.dart';
+import 'package:bookly_app/core/utlis/routers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -12,7 +14,9 @@ class CustomAppBar extends StatelessWidget {
         Image.asset(AppAssets.logo, height: 18),
         const Spacer(),
         IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(Routers.searchView);
+            },
             icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 22))
       ],
     );

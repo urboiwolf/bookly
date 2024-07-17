e4cdd611264e96ac9b59180d5b2791a78a27a54a
anas <anas3ez@gmail.com>
2024-07-17 14:25:37 +0300

    set up search screen

--- lib/core/utlis/routers.dart
+++ lib/core/utlis/routers.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentaion/views/book_details_screen.dart';
 import 'package:bookly_app/features/home/presentaion/views/home_screen.dart';
+import 'package:bookly_app/features/search/presentaion/views/search_screen.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,6 +9,7 @@ abstract class Routers {
   static const splashView = '/';
   static const homeView = '/homeView';
   static const bookDetails = '/bookDetails';
+  static const searchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -17,6 +19,10 @@ abstract class Routers {
       GoRoute(
         path: homeView,
         builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: searchView,
+        builder: (context, state) => const SearchScreen(),
       ),
       GoRoute(
         path: bookDetails,
